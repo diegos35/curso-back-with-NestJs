5219f46c29bb180f4dfd3b1e4683aced7ad7f704
diego caviedes <diego_1996.c.c@hotmail.com>
2023-03-10 16:45:35 -0500

    introduction to controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'soy new endpoint';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'soy new endpoint';
+  }
 }
